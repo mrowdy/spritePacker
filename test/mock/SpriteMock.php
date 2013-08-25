ddab2cf821494a1cede7910a62ccae7fb32b0c4c
Slemgrim <slemgrim@gmail.com>
2013-08-25 15:37:45 +0200

    sprite mock object

--- test/mock/SpriteMock.php
+++ test/mock/SpriteMock.php
@@ -1,2 +1,34 @@
 <?php
 
+class SpriteMock implements iSprite{
+
+    private $imageWidth;
+    private $imageHeight;
+    private $spriteWidth;
+    private $spriteHeight;
+
+    public function __construct($imageWidth, $imageHeight, $spriteWidth, $spirteHeight){
+        $this->imageWidth = $imageWidth;
+        $this->imageHeight = $imageHeight;
+        $this->spriteWidth = $spriteWidth;
+        $this->spriteHeight = $spirteHeight;
+    }
+
+    public function getImageWidth(){
+        return $this->imageWidth;
+    }
+
+    public function getImageHeight(){
+        return $this->imageHeight;
+    }
+
+    public function getSpriteWidth(){
+        return $this->spriteWidth;
+    }
+
+    public function getSpriteHeight(){
+        return $this->spriteHeight;
+        return $this->spriteHeight;
+    }
+
+}
