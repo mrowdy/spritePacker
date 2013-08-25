05c79710b3969cb98ee7fec082fb59dc98c69857
Slemgrim <slemgrim@gmail.com>
2013-08-25 15:37:32 +0200

    sprite interface

---
+++ iSprite.php
@@ -0,0 +1,10 @@
+<?php
+
+interface iSprite {
+
+    public function getImageWidth();
+    public function getImageHeight();
+    public function getSpriteWidth();
+    public function getSpriteHeight();
+
+}
