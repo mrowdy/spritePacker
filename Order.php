5dab72dbe1122f948be86be28bcda650e4eaf45c
Slemgrim <slemgrim@gmail.com>
2013-08-25 16:36:12 +0200

    order by spritesize or image size

--- Order.php
+++ Order.php
@@ -2,7 +2,15 @@
 
 class Order {
 
+    const ORDER_BY_IMAGESIZE = 0;
+    const ORDER_BY_SPRITESIZE = 1;
+    protected static $orderBy;
+
     protected $sprites = array();
+
+    public function __construct(){
+        self::$orderBy = self::ORDER_BY_IMAGESIZE;
+    }
 
     public function getSprites(){
         return $this->sprites;
@@ -12,21 +20,52 @@ class Order {
         array_push($this->sprites, $sprite);
     }
 
+    public function addSprites($sprites){
+        if(is_array($sprites)){
+            foreach($sprites as $sprite){
+                $this->addSprite($sprite);
+            }
+        } else {
+            $this->addSprite($sprites);
+        }
+    }
+
     public function order(){
         if(!empty($this->sprites)){
             usort($this->sprites, array('Order', 'orderLargestToSmallest'));
         }
     }
 
+    public function setOrderBy($orderBy){
+        switch($orderBy){
+            case self::ORDER_BY_SPRITESIZE:
+                self::$orderBy = self::ORDER_BY_SPRITESIZE;
+                break;
+            case self::ORDER_BY_IMAGESIZE:
+            default:
+                self::$orderBy = self::ORDER_BY_IMAGESIZE;
+                break;
+        }
+    }
+
     protected static function orderLargestToSmallest($a, $b){
 
-        $aSize = $a->getImageWidth() * $a->getImageHeight();
-        $bSize = $b->getImageWidth() * $b->getImageHeight();
+        switch(self::$orderBy){
+            case self::ORDER_BY_SPRITESIZE:
+                $aSize = $a->getImageWidth() * $a->getImageHeight();
+                $bSize = $b->getImageWidth() * $b->getImageHeight();
+                break;
+            case self::ORDER_BY_IMAGESIZE:
+            default:
+                $aSize = $a->getSpriteWidth() * $a->getSpriteHeight();
+                $bSize = $b->getSpriteWidth() * $b->getSpriteHeight();
+                break;
+        }
 
         if ($aSize == $bSize) {
             return 0;
         }
-        return ($aSize > $bSize) ? -1 : 1;
+        return ($aSize < $bSize) ? -1 : 1;
 
     }
 }
--- test/OrderTest.php
+++ test/OrderTest.php
@@ -36,19 +36,108 @@ class OrderTest extends PHPUnit_Framework_TestCase {
         $order->order();
     }
 
-    public function testOrder_orderTwoSprites_largestToSmallest(){
-        $sprite2 = new SpriteMock(120, 120, 100, 100);
-        $sprite1 = new SpriteMock(100, 100, 80, 80);
-
-
+    public function testOrder_addTwoSingleSprites_largestToSmallest(){
         $order = new Order();
+        $sprite1 = new SpriteMock(120, 120, 100, 100);
+        $sprite2 = new SpriteMock(130, 130, 100, 100);
+
         $order->addSprite($sprite1);
         $order->addSprite($sprite2);
+
         $order->order();
+        $orderedSprites = $order->getSprites();
 
-        $sprites = $order->getSprites();
-        $this->assertSame($sprites[0], $sprite2);
-        $this->assertSame($sprites[1], $sprite1);
+        $this->assertSame($orderedSprites[0], $sprite2);
+        $this->assertSame($orderedSprites[1], $sprite1);
+    }
 
+    public function testOrder_addTwoSingleSprites_largestToSmallestBySpriteSize(){
+        $order = new Order();
+        $sprite1 = new SpriteMock(120, 120, 120, 120);
+        $sprite2 = new SpriteMock(130, 130, 100, 100);
+
+        $order->addSprite($sprite1);
+        $order->addSprite($sprite2);
+
+        $order->setOrderBy(Order::ORDER_BY_SPRITESIZE);
+        $order->order();
+        $orderedSprites = $order->getSprites();
+
+        $this->assertSame($orderedSprites[0], $sprite1);
+        $this->assertSame($orderedSprites[1], $sprite2);
+    }
+
+    public function testOrder_orderTwoSprites_largestToSmallest(){
+        $sprites = array(
+            new SpriteMock(130, 130, 50, 50),
+            new SpriteMock(120, 120, 40, 40),
+        );
+
+        $order = new Order();
+        $order->addSprites($sprites);
+
+        $order->order();
+        $orderedSprites = $order->getSprites();
+
+        $this->assertSame($orderedSprites[0], $sprites[1]);
+        $this->assertSame($orderedSprites[1], $sprites[0]);
+    }
+
+    public function testOrder_orderFourSprites_largestToSmallest(){
+        $sprites = array(
+            new SpriteMock(100, 100, 50, 50),
+            new SpriteMock(120, 120, 40, 40),
+            new SpriteMock(130, 130, 30, 30),
+            new SpriteMock(140, 140, 20, 20)
+        );
+
+        $order = new Order();
+        $order->addSprites($sprites);
+
+        $order->order();
+        $orderedSprites = $order->getSprites();
+
+        $this->assertSame($orderedSprites[0], $sprites[3]);
+        $this->assertSame($orderedSprites[1], $sprites[2]);
+        $this->assertSame($orderedSprites[2], $sprites[1]);
+        $this->assertSame($orderedSprites[3], $sprites[0]);
+    }
+
+    public function testOrder_orderTwoBySpriteSize_LargestToSmallest(){
+        $sprites = array(
+            new SpriteMock(100, 100, 50, 50),
+            new SpriteMock(120, 120, 40, 40),
+        );
+
+        $order = new Order();
+        $order->addSprites($sprites);
+
+        $order->setOrderBy(Order::ORDER_BY_SPRITESIZE);
+        $order->order();
+        $orderedSprites = $order->getSprites();
+
+        $this->assertSame($orderedSprites[0], $sprites[0]);
+        $this->assertSame($orderedSprites[1], $sprites[1]);
+    }
+
+    public function testOrder_ordeFourBySpriteSize_LargestToSmallest(){
+        $sprites = array(
+            new SpriteMock(100, 100, 50, 50),
+            new SpriteMock(120, 120, 40, 40),
+            new SpriteMock(130, 130, 30, 30),
+            new SpriteMock(140, 140, 20, 20)
+        );
+
+        $order = new Order();
+        $order->addSprites($sprites);
+
+        $order->setOrderBy(Order::ORDER_BY_SPRITESIZE);
+        $order->order();
+        $orderedSprites = $order->getSprites();
+
+        $this->assertSame($orderedSprites[0], $sprites[0]);
+        $this->assertSame($orderedSprites[1], $sprites[1]);
+        $this->assertSame($orderedSprites[2], $sprites[2]);
+        $this->assertSame($orderedSprites[3], $sprites[3]);
     }
 }
