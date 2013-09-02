cd42ac89de94d25a2b8d3f65e188f9647f1ad851
Slemgrim <slemgrim@gmail.com>
2013-09-02 19:56:36 +0200

    use new sprites in example

--- index.php
+++ index.php
@@ -6,22 +6,22 @@ ini_set("display_errors", 1);
 require_once 'SpritePacker.php';
 
 $options = array(
-    'atlas-width' => 1600,
-    'atlas-height' => 2000,
+    'atlas-width' => 500,
+    'atlas-height' => 500,
 );
 
 $spritePacker = new SpritePacker($options);
 
 for($i = 0; $i < 8; $i++){
-    $spritePacker->addSprite('test/sprites/test1.png');
+    $spritePacker->addSprite('test/sprites/testSprite1.png');
 }
 
 for($i = 0; $i < 3; $i++){
-    $spritePacker->addSprite('test/sprites/test1x2.png');
+    $spritePacker->addSprite('test/sprites/testSprite2.png');
 }
 
 for($i = 0; $i < 2; $i++){
-    $spritePacker->addSprite('test/sprites/test1x4.png');
+    $spritePacker->addSprite('test/sprites/testSprite3.png');
 }
 
 $spritePacker->run();
