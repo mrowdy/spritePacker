b71018951a87d26d35ce2626cea338054f78a178
Slemgrim <slemgrim@gmail.com>
2013-09-02 19:44:41 +0200

    replace test sprites

--- test/SpriteTest.php
+++ test/SpriteTest.php
@@ -6,6 +6,10 @@ require_once '../Sprite.php';
 
 class SpriteTest extends PHPUnit_Framework_TestCase {
 
+    private $sprite32x32 = 'sprites/testSprite1.png';
+    private $sprite64x64 = 'sprites/testSprite2.png';
+    private $sprite128x128 = 'sprites/testSprite3.png';
+
     /**
      * @expectedException Exception
      */
@@ -14,22 +18,22 @@ class SpriteTest extends PHPUnit_Framework_TestCase {
     }
 
     public function testSprite_ConstructValidImage(){
-        $path = 'sprites/test1.png';
+        $path = $this->sprite32x32;
         $sprite = new Sprite($path);
         $this->assertEquals($path, $sprite->getSpritePath());
     }
 
     public function testSprite_loadValidImage_correctImageDimensions(){
-        $expectedWidth = 200;
-        $expectedHeight = 200;
-        $sprite = new Sprite('sprites/test1.png');
+        $expectedWidth = 32;
+        $expectedHeight = 32;
+        $sprite = new Sprite($this->sprite32x32);
         $this->assertEquals($expectedWidth, $sprite->getImageWidth());
         $this->assertEquals($expectedHeight, $sprite->getImageHeight());
     }
 
     public function testSprite_getMimeType_png(){
         $expected = 'image/png';
-        $sprite = new Sprite('sprites/test1.png');
+        $sprite = new Sprite($this->sprite32x32);
 
         $this->assertEquals($expected, $sprite->getMimeType());
     }
@@ -37,41 +41,41 @@ class SpriteTest extends PHPUnit_Framework_TestCase {
 
     public function testSprite_getImageType_png(){
         $expected = '3';
-        $sprite = new Sprite('sprites/test1.png');
+        $sprite = new Sprite($this->sprite32x32);
 
         $this->assertEquals($expected, $sprite->getImageType());
     }
 
     public function testSprite_getSpriteTopX(){
-        $expected = '5';
-        $sprite = new Sprite('sprites/test1.png');
+        $expected = '1';
+        $sprite = new Sprite($this->sprite32x32);
 
         $this->assertEquals($expected, $sprite->getSpriteTopX());
     }
 
     public function testSprite_getSpriteTopY(){
-        $expected = '13';
-        $sprite = new Sprite('sprites/test1.png');
+        $expected = '1';
+        $sprite = new Sprite($this->sprite32x32);
 
         $this->assertEquals($expected, $sprite->getSpriteTopY());
     }
 
     public function testSprite_getSpriteWidth(){
-        $expected = '190';
-        $sprite = new Sprite('sprites/test1.png');
+        $expected = '30';
+        $sprite = new Sprite($this->sprite32x32);
 
         $this->assertEquals($expected, $sprite->getSpriteWidth());
     }
 
     public function testSprite_getSpriteHeight(){
-        $expected = '176';
-        $sprite = new Sprite('sprites/test1.png');
+        $expected = '30';
+        $sprite = new Sprite($this->sprite32x32);
 
         $this->assertEquals($expected, $sprite->getSpriteHeight());
     }
 
     public function testSprite_getImage_resource(){
-        $sprite = new Sprite('sprites/test1.png');
+        $sprite = new Sprite($this->sprite32x32);
         $this->assertEquals('resource', gettype($sprite->getImage()));
     }
 }
