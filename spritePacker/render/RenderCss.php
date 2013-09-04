d8cf2b5b9ff147727b486df479f3a97bfac1a474
Slemgrim <slemgrim@gmail.com>
2013-09-04 21:40:35 +0200

    add css renderer

---
+++ spritePacker/render/RenderCss.php
@@ -0,0 +1,63 @@
+<?php
+
+class RenderCSS implements iRenderer {
+
+    protected $css;
+    protected $path;
+
+    public function __construct($path){
+        $this->path = $path;
+    }
+
+    public function render(Atlas $atlas, array $sprites){
+        $this->createSpriteClass($atlas);
+        $this->createSprites($sprites);
+    }
+
+    public function show(){
+        echo $this->css;
+    }
+
+    public function save(){
+        file_put_contents($this->path, $this->css);
+    }
+
+
+    /**
+     * TODO: replace hardcoded path with atlas name
+     */
+    public function createSpriteClass($atlas){
+        $this->css = sprintf('
+            .sprite {
+                background: transparent url(%s);
+                width: %dpx;
+                height: %dpx;
+            }
+        ', 'atlas.png', $atlas->width, $atlas->height);
+    }
+
+    public function createSprites($sprites){
+        foreach($sprites as $sprite){
+            $spriteCss = sprintf('
+                .sprite.%s {
+                    width: %dpx;
+                    height: %dpx;
+                    background-position: -%dpx -%dpx;
+                    display: block;
+                    padding: 0px;
+                }',
+                $sprite->getName(),
+                $sprite->getAtlasPositionWidth(),
+                $sprite->getAtlasPositionHeight(),
+                $sprite->getAtlasPositionX(),
+                $sprite->getAtlasPositionY());
+
+            $this->css .= $spriteCss;
+        }
+
+
+
+    }
+
+
+}
